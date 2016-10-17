eed3ee3fad7fe16a7cc96f53091d441d0f3f1157
TonyUCLA <TonyUCLA>
2016-10-17 14:00:05 -0700

    Psuedo Code for javascript half done

---
+++ assets/javascript/game.js
@@ -0,0 +1,86 @@
+// Pseudo Code for Star Wars RPG
+
+/* The player will choose a character by clicking on a picture.
+Once selected, the player will fight as that character for the rest of the game. */
+
+// Once a player is selected the remaining characters will be moved down to "Enemies" section.
+/* The player will then choose an enemy by clicking on a picture. That enemy will then be moved to the "Defender" Section. */
+
+
+
+
+$(document).ready(function() {
+
+// var myGameCharacters = [
+// 	$('#gimli.character').data({
+// 		health: 150,
+// 		name: 'Gimli',
+// 		attack: 100
+// 	}),
+// 	$('#arwen.character').data({
+// 		health: 100,
+// 		name: 'Arwen',
+// 		attack: 455
+// 	}),
+// 	$('#frodo.character').data({
+// 		health: 50,
+// 		name: 'Frodo',
+// 		attack: 5
+// 	})
+// ];
+
+// $('.character').on('click', function() {
+// 	var $currentCharacter = $(this);
+//   var characterData = $currentCharacter.data();
+//   console.log(characterData)
+//   $('#output').html(
+//   		'<h1>Name: ' + characterData.name +'</h1>' +
+//       	'<h2>Attack: ' + characterData.attack +'</h2>'
+//   );
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
