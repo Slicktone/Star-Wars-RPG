d8de388574da6ec904f42c96ddc350d14f7d137a
TonyUCLA <TonyUCLA>
2016-10-18 19:15:38 -0700

    Got the characters to move

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,81 +6,56 @@ Once selected, the player will fight as that character for the rest of the game.
 // Once a player is selected the remaining characters will be moved down to "Enemies" section.
 /* The player will then choose an enemy by clicking on a picture. That enemy will then be moved to the "Defender" Section. */
 
-
+// Create an array and an object that will access their health, power, and character portrait.
 
 
 $(document).ready(function() {
 
-// var myGameCharacters = [
-// 	$('#gimli.character').data({
-// 		health: 150,
-// 		name: 'Gimli',
-// 		attack: 100
-// 	}),
-// 	$('#arwen.character').data({
-// 		health: 100,
-// 		name: 'Arwen',
-// 		attack: 455
-// 	}),
-// 	$('#frodo.character').data({
-// 		health: 50,
-// 		name: 'Frodo',
-// 		attack: 5
-// 	})
-// ];
+var obiWan, luke, darthSid, maul, userChar, enemies, fightSection, defender;
+var isSet = false;
 
-// $('.character').on('click', function() {
-// 	var $currentCharacter = $(this);
-//   var characterData = $currentCharacter.data();
-//   console.log(characterData)
-//   $('#output').html(
-//   		'<h1>Name: ' + characterData.name +'</h1>' +
-//       	'<h2>Attack: ' + characterData.attack +'</h2>'
-//   );
-// })
+var myGameCharacters = [
+	$("#obiwan.character").data({
+		health: 200,
+		attack: 200,
+		counterAttack: 0,
+	}),
+	$("#luke.character").data({
+		health: 300,
+		attack: 300,
+		counterAttack: 0,
+	}),
+	$("#darthsid.character").data({
+		health: 500,
+		attack: 500,
+		counterAttack: 0,
+	}),
+	$("#maul.character").data({
+		health: 400,
+		attack: 400,
+		counterAttack: 0,
+	})
 
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".character").on("click", function selectedChar() {
+	// isSet is declared to false at the top.
+	// setting the isSet variable to not true, so the other characters don't move.
+	if(!isSet) { // if selected character is not the variable isSet, set it equal to true.
+		isSet = true;
+		$('#userchar').append($(this));
+		// when a user clicks on a character, it will remove the class and append it.
+	$(this).attr("class","currentCharacter")
+		// this will take the rest of the characters and move them to the enemies sections.
+		// appendTo will add 
+		$(".character").appendTo("#enemies")
+		}
+})
 
 
+// $(".character").on("click", function(){
+// 	$("#userchar").append();
+		// $("#obiwan").empty();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		
 })
